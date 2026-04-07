a986a2180e10a19b7e94f96943e551a2c2317c7e
simeon-issanga <simeonissanga33@gmail.com>
2026-04-07 15:31:26 +0200

    Update EmplacementDAO.java

--- EmplacementDAO.java
+++ EmplacementDAO.java
@@ -1,11 +1,12 @@
-package com.example.projetandroid;
+package com.example.projetandroid.classesDAO;
 
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
-import androidx.room.Transaction;
+
+import com.example.projetandroid.classes.Emplacement;
 
 import java.util.List;
 
