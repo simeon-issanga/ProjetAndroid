fd800f8a024cd6fb6ce20114b0da5512cfdd20d9
laifa001 <123881257+laifasyoud01@users.noreply.github.com>
2026-04-16 11:15:24 +0200

    Add DAO methods for Personne entity

--- PersonneDAO.java
+++ PersonneDAO.java
@@ -1,4 +1,32 @@
 package com.example.projetandroid.classesDAO;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.projetandroid.classes.Personne;
+
+import java.util.List;
+
+@Dao
 public interface PersonneDAO {
+    
+    @Query("SELECT * FROM Personne")
+    LiveData<List<Personne>> getAll();
+    @Query("SELECT * FROM Personne WHERE idP = :unId")
+    LiveData<Personne> getPersonneById(long unId);
+    
+    @Insert
+    void insert(Personne unePersonne);
+    
+    @Update
+    void update(Personne unePersonne);
+
+  
+    @Delete
+    void delete(Personne unePersonne);
 }
+
